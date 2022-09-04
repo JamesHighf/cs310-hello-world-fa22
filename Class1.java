6b58b009b3e585105452d57ec95be978fb8eabba
JamesHighf <jameschighfield@gmail.com>
2022-09-04 11:57:17 -0500

    Started work on f4()

--- Class1.java
+++ Class1.java
@@ -11,7 +11,9 @@ public class Class1 {
     private void f2() {
         // v2.0
     }
-    private void f2() {
+    private void f3() {
         // v3.0
+    private void f4() {
+       // v3.0
     }
 }
